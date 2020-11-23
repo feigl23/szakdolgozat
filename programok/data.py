182ef42560a19e3f8816c60d4e85e63b04e7e3ae
feigl23 <feigledit23@gmail.com>
2020-11-23 21:21:09 +0100

    positions

--- programok/data.py
+++ programok/data.py
@@ -1,12 +1,17 @@
 p_model_data={ # ezek az adatok a kek pingvin kezdoadatai voltak a demoban
-    "x" :0, #x kezdopizicio
-    "y":3.2, #y kezdopozicio
-    "z" :5.5, #z kezdopizicio
+    "positions":((0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5),(0,3.2,5.5),(0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),(0,3.2,5.5),(0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5) ,(0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5),(0,3.2,5.5),(0,3.2,5.5),(0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),(0,3.2,5.5),(0,3.2,5.5),
+    (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5),
+    (0,3.2,5.5),(0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5), (0,3.2,5.5)),
+    (0,3.2,5.5)),
     "rot_z" : 0, #elforgatas
-    'model': None, # az a modell ami akkor van felhasznalva, ha nincs animacio
-    "walk_models" : [], #seta animacio modelljei
-    "jump_models" : [], #ugras animacio modelljei
-    "grab_models" : [], #a tolas animacio modelljei
     "axis" : "y", # a tengely amin epp mozog
     "dist" : 0, #a tavolsag amit megtesz egy animacio soran
     "const_dist" :0, # a tavolsag ugyszinten, de mivel a dist folyamatosan csökken ez a visszaallitasban segit
@@ -19,11 +24,9 @@ p_model_data={ # ezek az adatok a kek pingvin kezdoadatai voltak a demoban
 }
 
 box_data={
-"model" : [], #a modellek tombje
 "color" : [], #a modellek szine
-"x" : [-14,-10,-6,-2,2,6,10,14,-14,-10,-6,-2,2,6,10,14], # x kezdopizicioi a dobozoknak
-"y" : [13,-2,13,-2,13,-2,13,-2,-2,13,-2,13,-2,13,-2,13], # y kezdopizicioi a dobozoknak
-"z" : [30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30,30], # z kezdopizicioi a dobozoknak
+"positions" : ((-14,13,30),(-10,-2,30), (-6,13,30), (-2,-2,30), (2,13,30), (6,-2,30),
+(10, 13, 30), (14,-2,30), (-14,13,30), (-10,-2,30), (-6,13,30),(-2,-2,30), (2, 13, 30), (6,-2,30), (10,13,30), (14,-2,30)),
 "removed": [], # az eltuntetett dobozok id-ja
 "left": True, # azt jelzi aktiv e a bal kut
 "right":False, # azt jelzi aktiv e a jobb kut
